ba5f244984221261da38e969d7f48e032f8815ad
life4music <mawlodkowska@gmail.com>
2021-02-16 11:51:35 +0100

    news search

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const News = require('../models/news');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  // po przechwyceniu adresu strony głównej
-  res.render('news', { title: 'News' });
+  // pobieranie danych wyszukiwania
+  const search = req.query.search || '';
+
+  const findNews = News
+  //pobranie aktykułów i wyszukiwanie po tytule
+  .find({title: new RegExp(search.trim(), 'i')})  //trim zapewnia pomijanie spacji itd. przy wyszukiwaniu
+  //sortowanie po dacie
+  .sort({created:-1}) 
+
+  findNews.exec((err,data) => {
+    res.render('news', { title: 'News', data, search });
+  })
 });
 
 module.exports = router;
