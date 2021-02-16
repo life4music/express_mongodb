58d5e1e3f7659b85093078a5c874f8c4fed16fe3
life4music <mawlodkowska@gmail.com>
2021-02-16 13:46:47 +0100

    config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   db: 'mongodb+srv://admin:aIF0gC34TZ9Uzoud@cluster0.bnauz.mongodb.net/test?retryWrites=true&w=majority',
-  keySession: ['TWOJKLUCZ1'],
+  keySession: ['TWOJKLUCZ'],
   maxAgeSession: 24 * 60 * 60 * 1000 //24h
 }
